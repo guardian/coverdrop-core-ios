3d5b78b61487f105da6e723ad4e57a4f793a15b4
Secure Collaboration <secure.collaboration@theguardian.com>
2025-04-30 09:44:50 +0100

    Project import generated by Copybara.
    GitOrigin-RevId: 696c229ba5a81f1febe74820192fa5b2a67a5862

--- Sources/CoverDropCore/CoverDropConfig.swift
+++ Sources/CoverDropCore/CoverDropConfig.swift
@@ -16,28 +16,22 @@ public protocol CoverDropConfig {
 }
 
 public enum EnvType {
-    case dev, code, prod, staging, demo
+    case prod, staging, dev
 }
 
 public enum StaticConfig: CoverDropConfig {
-    case devConfig
-    case codeConfig
     case prodConfig
     case stagingConfig
-    case demoConfig
+    case devConfig
 
     private func internalGetConfig() -> CoverDropConfig {
         switch self {
-        case .devConfig:
-            return DevConfig()
-        case .codeConfig:
-            return CodeConfig()
         case .prodConfig:
             return ProdConfig()
         case .stagingConfig:
             return StagingConfig()
-        case .demoConfig:
-            return DemoConfig()
+        case .devConfig:
+            return DevConfig()
         }
     }
 
@@ -94,23 +88,6 @@ public struct ProdConfig: CoverDropConfig {
     public var numMessagesPerBackgroundRun = 2
 }
 
-public struct DemoConfig: CoverDropConfig {
-    public var envType: EnvType = .demo
-    public var withSecureDns: Bool = true
-
-    public var passphraseWordCount = 3
-
-    public let apiBaseUrl = "https://secure-messaging-api-demo.guardianapis.com/v1"
-    public let messageBaseUrl = "https://secure-messaging-msg-demo.guardianapis.com"
-
-    public let cacheEnabled = false
-
-    public let maxBackgroundDurationInSeconds = Constants.maxBackgroundDurationInSeconds
-    public var minDurationBetweenBackgroundRunsInSecs = 60 * 60
-    public var numMessagesPerBackgroundRun = 2
-    public var removeBackgroundSendStateOnStart = false
-}
-
 public struct StagingConfig: CoverDropConfig {
     public var envType: EnvType = .staging
     public var withSecureDns: Bool = true
@@ -123,23 +100,6 @@ public struct StagingConfig: CoverDropConfig {
     public let cacheEnabled = true
 
     public let maxBackgroundDurationInSeconds = Constants.maxBackgroundDurationInSeconds
-    public var minDurationBetweenBackgroundRunsInSecs = 60 * 60
-    public var numMessagesPerBackgroundRun = 2
-    public var removeBackgroundSendStateOnStart = false
-}
-
-public struct CodeConfig: CoverDropConfig {
-    public var envType: EnvType = .code
-    public var withSecureDns: Bool = true
-
-    public var passphraseWordCount = 3
-
-    public let apiBaseUrl = "https://coverdrop-api.code.dev-gutools.co.uk/v1"
-    public let messageBaseUrl = "https://secure-messaging.code.dev-guardianapis.com"
-
-    public let cacheEnabled = false
-
-    public let maxBackgroundDurationInSeconds = 10
     public var minDurationBetweenBackgroundRunsInSecs = 60 * 60
     public var numMessagesPerBackgroundRun = 2
     public var removeBackgroundSendStateOnStart = false
